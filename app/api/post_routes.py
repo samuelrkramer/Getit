79a12a1286203b8d11aba43d21faf26781fa8481
Sam Kramer <SamuelRKramer@gmail.com>
2022-06-09 14:35:32 -0400

    looks like a working PUT route too

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, jsonify
-from flask_login import login_required
+from flask import Blueprint, jsonify, request
+from flask_login import login_required, current_user
 from app.models import db, Post
 from app.forms import PostForm
 from .auth_routes import validation_errors_to_error_messages
@@ -11,10 +11,11 @@ post_routes = Blueprint('posts', __name__)
 @login_required
 def new_post():
     form = PostForm()
-    # form['csrf_token'].data = request.cookies['csrf_token']
+    form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
         post = Post(
-            userId=form.data['userId'],
+            # userId=form.data['userId'],
+            userId=current_user.id,
             title=form.data['title'],
             body=form.data['body']
         )
@@ -23,11 +24,31 @@ def new_post():
         return post.to_dict()
     return {'errors': validation_errors_to_error_messages(form.errors)}, 400
 
+
 @post_routes.route('/')
 # @login_required
 def posts():
     posts = Post.query.all()
     return {'posts': [post.to_dict() for post in posts]}
+
+
+@post_routes.route('/<int:id>', methods=['PUT'])
+@login_required
+def edit_post(id):
+    post = Post.query.get(id)
+    if not post:
+        return {'errors': ["not found"]}, 404
+    form = PostForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        if post.id != current_user.id:
+            return {'errors': ["not yours"]}, 403
+        post.title = form.data['title']
+        post.body=form.data['body']
+        # db.session.add(post)
+        db.session.commit()
+        return post.to_dict()
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 400
 
 
 @post_routes.route('/<int:id>')
