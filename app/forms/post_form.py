62d59eaa120a919f2f10977f96d2120617af0e78
Sam Kramer <SamuelRKramer@gmail.com>
2022-06-09 14:42:12 -0400

    comment out this stuff which will cause errors when I try to run it, and hope I remember to get rid of it later

--- app/forms/post_form.py
+++ app/forms/post_form.py
@@ -4,26 +4,28 @@ from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import Post
 
 
-def user_exists(form, field):
-    # Checking if user exists
-    email = field.data
-    user = User.query.filter(User.email == email).first()
-    if not user:
-        raise ValidationError('Email provided not found.')
+# def user_exists(form, field):
+#     # Checking if user exists
+#     email = field.data
+#     user = User.query.filter(User.email == email).first()
+#     if not user:
+#         raise ValidationError('Email provided not found.')
 
 
-def password_matches(form, field):
-    # Checking if password matches
-    password = field.data
-    email = form.data['email']
-    user = User.query.filter(User.email == email).first()
-    if not user:
-        raise ValidationError('No such user exists.')
-    if not user.check_password(password):
-        raise ValidationError('Password was incorrect.')
+# def password_matches(form, field):
+#     # Checking if password matches
+#     password = field.data
+#     email = form.data['email']
+#     user = User.query.filter(User.email == email).first()
+#     if not user:
+#         raise ValidationError('No such user exists.')
+#     if not user.check_password(password):
+#         raise ValidationError('Password was incorrect.')
+
+# here's a print() and a console.log() so I can search for this later
 
 
 class PostForm(FlaskForm):
-    userId = IntegerField('userId', validators=[DataRequired(), user_exists])
+    userId = IntegerField('userId' )#, validators=[DataRequired() ])#, user_exists])
     title = StringField('title', validators=[DataRequired()])
     body = TextAreaField('body')
