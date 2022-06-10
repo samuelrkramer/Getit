66ffec4df087a664f6a4ec5dde59d64af7077f40
Sam Kramer <SamuelRKramer@gmail.com>
2022-06-10 18:19:10 -0400

    change navbar based on logged in status, while I'm at it

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,25 +1,17 @@
 
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
 
 const NavBar = () => {
+  const user = useSelector(state => state.session.user)
   return (
     <nav>
       <ul>
         <li>
           <NavLink to='/' exact={true} activeClassName='active'>
             Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
           </NavLink>
         </li>
         <li>
@@ -32,9 +24,30 @@ const NavBar = () => {
             Posts
           </NavLink>
         </li>
-        <li>
-          <LogoutButton />
-        </li>
+        {user && (
+        <>
+          <li>
+            Logged in as <NavLink to={`/users/${user.id}`} >{user.username}</NavLink>
+          </li>
+          <li>
+            <LogoutButton />
+          </li>
+        </>
+        )}
+        {!user && (
+          <>
+            <li>
+              <NavLink to='/login' exact={true} activeClassName='active'>
+                Login
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/sign-up' exact={true} activeClassName='active'>
+                Sign Up
+              </NavLink>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
