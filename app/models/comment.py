9ef4743d3cc30b5e95b760a2c5f6544cdd82f049
Sam Kramer <SamuelRKramer@gmail.com>
2022-06-14 14:48:55 -0400

    setting up relationships

--- app/models/comment.py
+++ app/models/comment.py
@@ -1,3 +1,5 @@
+from sqlalchemy.orm import relationship
+from sqlalchemy.schema import ForeignKey
 from .db import db
 from datetime import datetime
 
@@ -6,14 +8,18 @@ class Comment(db.Model):
     __tablename__ = 'comments'
 
     id = db.Column(db.Integer, primary_key=True)
-    userId = db.Column(db.Integer, nullable=False)
-    postId = db.Column(db.Integer)
+    userId = db.Column(db.Integer, ForeignKey("users.id"), nullable=False)
+    postId = db.Column(db.Integer, ForeignKey("posts.id"))
     parentId = db.Column(db.Integer)
     body = db.Column(db.Text, nullable=False)
-    
+
     created_at = db.Column(db.DateTime, default=datetime.now())
     updated_at = db.Column(db.DateTime, default=datetime.now(), onupdate=datetime.now())
 
+    user = relationship("User")#, back_populates="posts")
+    post = relationship("Post")#, back_populates="comments")
+    # comments = relationship("Comment", back_populates="parent")
+    
     def to_dict(self):
         return {
             'id': self.id,
--- app/models/post.py
+++ app/models/post.py
@@ -1,3 +1,5 @@
+from sqlalchemy.orm import relationship
+from sqlalchemy.schema import ForeignKey
 from .db import db
 from datetime import datetime
 
@@ -6,12 +8,15 @@ class Post(db.Model):
     __tablename__ = 'posts'
 
     id = db.Column(db.Integer, primary_key=True)
-    userId = db.Column(db.Integer, nullable=False)
+    userId = db.Column(db.Integer, ForeignKey("users.id"), nullable=False)
     title = db.Column(db.String(255), nullable=False)
     body = db.Column(db.Text)
     
     created_at = db.Column(db.DateTime, default=datetime.now)
     updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+
+    user = relationship("User")#, back_populates="posts")
+    comments = relationship("Comment")#, back_populates="post")
 
     def to_dict(self):
         return {
