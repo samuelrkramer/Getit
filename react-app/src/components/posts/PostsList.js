b7d09b19e6d6774ee350a68feda3aae682b63ae5
Sam Kramer <SamuelRKramer@gmail.com>
2022-06-16 17:22:52 -0400

    post list looks pretty good

--- react-app/src/components/posts/PostsList.js
+++ react-app/src/components/posts/PostsList.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink, Link } from 'react-router-dom';
 import { getAllPosts } from '../../store/post';
+import './PostsList.css';
 
 function PostsList() {
   const dispatch = useDispatch();
@@ -23,17 +24,25 @@ function PostsList() {
   const postComponents = postIds.map(i => {
     const post = posts[i];
     return (
-      <li key={i}>
-        <NavLink to={`/posts/${post.id}`}>{post.title}</NavLink> from <Link to={`/users/${post.userId}`}>{post.user.username}</Link>
-      </li>
+      <div className="listRow" key={i}>
+        <span class="rowIndex">{i}</span>
+        <div className="rowItem">
+          <p className="title">
+            <NavLink to={`/posts/${post.id}`}>{post.title}</NavLink>
+          </p>
+          <p className="tagline">
+            submitted at {post.created_at} by <Link to={`/users/${post.userId}`}>{post.user.username}</Link>
+          </p>
+        </div>
+      </div>
     );
   });
 
   return (
-    <>
-      <h1>Post List: </h1>
+    <div className="postList">
+      {/* <h1>Post List: </h1> */}
       <ul>{postComponents}</ul>
-    </>
+    </div>
   );
 }
 
