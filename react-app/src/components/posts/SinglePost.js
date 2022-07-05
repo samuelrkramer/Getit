05afe79592e6737dc00be718847905c90a3d2d29
Sam Kramer <SamuelRKramer@gmail.com>
2022-07-05 19:52:53 -0400

    huh, it works. visiting a post directly from the URL works.

--- react-app/src/components/posts/SinglePost.js
+++ react-app/src/components/posts/SinglePost.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams, Link } from 'react-router-dom';
+import { getOnePost } from '../../store/post';
 import { getPostsComments } from '../../store/comment';
 import PostHeader from './PostHeader';
 import CommentForm from './CommentForm';
@@ -20,16 +21,17 @@ function SinglePost() {
   
   const user = useSelector(state => state.session.user) || false;
 
-  useEffect(() => {
+  useEffect(async () => {
     if (!post) {
-      return ("Loading...");
+      // return ("Loading...");
+      dispatch(getOnePost(postId))
     }
     // (async () => {
     //   const response = await fetch(`/api/posts/${postId}`);
     //   const post = await response.json();
     //   setPost(post);
     // })();
-  }, [post]);
+  }, [dispatch]);
 
   useEffect(() => {
     dispatch(getPostsComments(postId))
