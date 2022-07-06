54d5097b333cba2e06cead4f6cc1ae11dba9a6e7
Sam Kramer <SamuelRKramer@gmail.com>
2022-07-06 14:47:31 -0400

    implement context and provider for sidebar

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,13 +4,16 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import configureStore from './store';
+import { SidebarContext } from './components/context/SidebarContext';
 
 const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-        <App />
+        <SidebarContext.Provider value={{ sidebar: "<div>default test</div>" }}>
+          <App />
+        </SidebarContext.Provider>
       </Provider>
   </React.StrictMode>,
   document.getElementById('root')
