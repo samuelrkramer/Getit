cd6ddf3859224b8ce084fc49419e68fd290afa3b
Sam Kramer <SamuelRKramer@gmail.com>
2022-07-13 15:56:49 -0400

    make the sidebar search actually go somewhere now

--- react-app/src/components/Sidebar.js
+++ react-app/src/components/Sidebar.js
@@ -1,18 +1,34 @@
 // import { useSelector } from "react-redux";
 // import LoginForm from "./auth/LoginForm";
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import { useHistory } from "react-router-dom";
 import { GetitContext } from "./context/GetitContext";
 import Logo from "./Logo";
 
 const Sidebar = () => {
   const { sideAdd } = useContext(GetitContext);
+
+  const [query, setQuery] = useState("");
+  const history = useHistory();
+
+  const searchHandler = e => {
+    e.preventDefault();
+    console.log(query, e);
+    history.push(`/search?query=${encodeURIComponent(query)}`);
+  }
+
   return (
     <div className="sidebar">
       <div className="sideSpaced">
-        <input type="text" 
-          value="Search coming soon"
-          disabled={true}
-        />
+        <form action="/search" method="GET" onSubmit={searchHandler}>
+          <input type="text"
+            name="query"
+            placeholder="Search GETit"
+            value={query}
+            onChange={e => setQuery(e.target.value)}
+            // disabled={true}
+          />
+        </form>
       Here's sidebar stuff
       </div>
       {sideAdd.map(el => (el))}
