c77bdf3a30326af750ae9c7d1775c36b506ba0e9
Sam Kramer <SamuelRKramer@gmail.com>
2022-07-13 00:07:51 -0400

    sort by descending

--- app/api/search_routes.py
+++ app/api/search_routes.py
@@ -11,7 +11,7 @@ search_routes = Blueprint('search', __name__)
 # @login_required
 def search():
     query = request.args.get('query')
-    posts = Post.query.filter(Post.title.ilike(f'%{query}%'))
+    posts = Post.query.filter(Post.title.ilike(f'%{query}%')).order_by(Post.created_at.desc())
     # comments = Comment.query.filter(Comment.body.ilike(f'%{query}%'))
     return {
         'posts': [post.to_dict() for post in posts],
