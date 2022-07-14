073c7711ff7e5ab0ed67e222563bbc44c039b378
Sam Kramer <SamuelRKramer@gmail.com>
2022-07-14 15:22:16 -0400

    modal attempt... prep?

--- react-app/src/context/GetitContext.js
+++ react-app/src/context/GetitContext.js
@@ -1,14 +1,41 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState, useRef, useEffect } from "react";
+import ReactDOM from 'react-dom';
 
 export const GetitContext = createContext();
 
 const GetitProvider = props => {
+  const modalRef = useRef();
+  const [modalVal, setModalVal] = useState();
+
   // const dummy = (<div>dummy sidebar context stuff</div>);
   const [sideAdd, setSideAdd] = useState([]);
+
+  useEffect(() => {
+    setModalVal(modalRef.current);
+  }, []);
+
   return (
-    <GetitContext.Provider value={{ sideAdd, setSideAdd }}>
-      {props.children}
-    </GetitContext.Provider>
+    <>
+      <GetitContext.Provider value={{ modalVal, sideAdd, setSideAdd }}>
+        {props.children}
+      </GetitContext.Provider>
+      <div ref={modalRef} />
+    </>
+  );
+};
+
+export const Modal = ({onClose, children}) => {
+  const {modalVal} = useContext(GetitContext);
+  if (!modalVal) return null;
+
+  return ReactDOM.createPortal(
+    <div id="modal">
+      <div id="modal-background" onClick={onClose} />
+      <div id="modal-content">
+        {children}
+      </div>
+    </div>,
+    modalVal
   );
 };
 
