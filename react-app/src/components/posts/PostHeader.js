bcb1b7fd510a0f19208bf17fd2971ee41184dbfd
Sam Kramer <SamuelRKramer@gmail.com>
2022-07-24 23:17:53 -0400

    make another line in the postheader with number of comments and... I guess a share link just to have more than one thing

--- react-app/src/components/posts/PostHeader.js
+++ react-app/src/components/posts/PostHeader.js
@@ -7,11 +7,19 @@ const PostHeader = ({ post, i=null }) => {
   const now = new Date();
   const createDate = new Date(Date.parse(post.created_at));
   // const editDate = new Date(Date.parse(post.updated_at));
+  const postUrl = `/posts/${post.id}`;
+  const numComments = Object.keys(post.comments).length;
   let edited = false;
   let dateString = post.created_at;
   if (post.updated_at !== post.created_at) {
     dateString += ", edited "+post.updated_at;
     edited = true;
+  }
+
+  const shareHandler = e => {
+    e.preventDefault();
+    navigator.clipboard.writeText(`https://skgetit.herokuapp.com${postUrl}`);
+    e.target.innerHTML = "copied"
   }
   return (
     <div className="listRow" key={i}>
@@ -22,13 +30,21 @@ const PostHeader = ({ post, i=null }) => {
         <p className="title">
           {/* {i && (<NavLink to={`/posts/${post.id}`}>{post.title}</NavLink>)} */}
           {/* {!i && post.title} */}
-          <Link to={`/posts/${post.id}`}>{post.title}</Link>
+          <Link to={postUrl}>{post.title}</Link>
         </p>
         <p className="tagline">
           submitted <span title={dateString}>
             {humanizeDuration(now-createDate, { largest: 1 } )} ago{edited?"*":""}
           </span> by <Link to={`/users/${post.userId}`}>{post.user.username}</Link>
         </p>
+        <ul className="thingMenu">
+          <li>
+            <Link to={postUrl}>{numComments || ""} comment{numComments>1 && "s"}</Link>
+          </li>
+          <li>
+            <a onClick={shareHandler}>share</a>
+          </li>
+        </ul>
       </div>
     </div>
   );
