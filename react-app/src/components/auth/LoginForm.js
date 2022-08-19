10a9eee1acb24cf8ff1b409082be838e5bc417ec
Sam Kramer <SamuelRKramer@gmail.com>
2022-08-19 03:06:58 -0400

    demo login button on signup page, and login and signup should link to each other

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { login, loginDemo } from '../../store/session';
 
 const LoginForm = () => {
@@ -68,6 +68,7 @@ const LoginForm = () => {
         />
         <button type='submit'>Login</button>
         <button onClick={demoLogin}>Demo</button>
+        <span className="xsmall"> Don't have an account? <Link to="/sign-up">Sign Up</Link></span>
       </div>
     </form>
   );
