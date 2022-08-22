0c55a7285fcb4305705e61f723a035af368546cb
Sam Kramer <SamuelRKramer@gmail.com>
2022-08-22 17:37:00 -0400

    visually look good for comment votes

--- react-app/src/components/posts/ShowComment.js
+++ react-app/src/components/posts/ShowComment.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import CommentForm from './CommentForm';
@@ -15,34 +16,62 @@ const ShowComment = ({comment, cForm, setCForm}) => {
     dateString += ", edited "+comment.updated_at;
     edited = true;
   }
+
+  const [score, setScore] = useState(comment.score);
+  const [vote, setVote] = useState(comment.myVote?.value || 0);
+
+  const voteClick = async (val) => {
+    setScore(score-vote+(vote!==val?val:0));
+    setVote(vote!==val?val:0);
+
+    // if (!user) return history.push("/sign-up");
+    // const result = await dispatch(postVote(post.id, vote!==val?val:0, post.myVote));
+    // if (result && result.errors) {
+    //   for (let err of result.errors) {
+    //     window.alert(err);
+    //   }
+    // }
+    // else {
+    //   setScore(score-vote+(vote!==val?val:0));
+    //   setVote(result.value || 0);
+    // }
+  }
+
   return (
   <div key={comment.id} className="oneComment">
     <a name={`c${comment.id}`} />
     {cForm === comment.id && (<CommentForm mode="Edit" postId={comment.postId} comment={comment} setCForm={setCForm} />)}
     {cForm !== comment.id && (
       <>
+        <div className={`voteBox voted${vote}`}>
+          <div className="voteArrow voteUp" onClick={() => voteClick(1)}>⬆</div>
+          {/* <span className="voteScore">{score}</span> */}
+          <div className="voteArrow voteDown" onClick={() => voteClick(-1)}>⬇</div>
+        </div>
         {/* <li><i>Comment ID: {comment.id}</i></li> */}
-        <p className="tagline">
-          <Link to={`/users/${comment.userId}`}>{comment.user.username}</Link> <span title={dateString}>
-            {humanizeDuration(now-createDate, { largest: 1 } )} ago{edited?"*":""}
-          </span>
-          { comment.userId === user.id && (<a onClick={() => setCForm(comment.id)} className="xsmall minMarg">Edit</a>)}
-        </p>
-        <ReactMarkdown className="commBody">{comment.body}</ReactMarkdown>
-        {/* <li><i>From: <Link to={`/users/${comment.userId}`}>{comment.user.username}</Link></i></li> */}
-        <ul className="thingMenu">
-          <li>
-            <Link to={`/posts/${comment.postId}#c${comment.id}`}>permalink</Link>
-          </li>
-          {/* <li>
-            <a onClick={shareHandler}>share</a>
-          </li> */}
-          {comment.userId === user.id && (
+        <div className="commentStuff">
+          <p className="tagline">
+            <Link to={`/users/${comment.userId}`}>{comment.user.username}</Link> <span title={dateString}>
+              {humanizeDuration(now-createDate, { largest: 1 } )} ago{edited?"*":""}
+            </span>
+            { comment.userId === user.id && (<a onClick={() => setCForm(comment.id)} className="xsmall minMarg">Edit</a>)}
+          </p>
+          <ReactMarkdown className="commBody">{comment.body}</ReactMarkdown>
+          {/* <li><i>From: <Link to={`/users/${comment.userId}`}>{comment.user.username}</Link></i></li> */}
+          <ul className="thingMenu">
             <li>
-              <a onClick={() => setCForm(comment.id)}>edit</a>
+              <Link to={`/posts/${comment.postId}#c${comment.id}`}>permalink</Link>
             </li>
-          )}
-        </ul>
+            {/* <li>
+              <a onClick={shareHandler}>share</a>
+            </li> */}
+            {comment.userId === user.id && (
+              <li>
+                <a onClick={() => setCForm(comment.id)}>edit</a>
+              </li>
+            )}
+          </ul>
+        </div>
       </>
     )}
   </div>
