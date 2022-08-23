samuelrkramer/Getit

commit 9e31bdeba13f307f8a3c380558d6306c59ea5931

just copy the post/vote routes and change postIds to commentIds and vice verse. this strangely works

app/api/comment_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
-from app.models import db, Post, Comment
-from app.forms import CommentForm
+from app.models import db, Comment, Vote
+from app.forms import CommentForm, VoteForm
 from .auth_routes import validation_errors_to_error_messages
 
 comment_routes = Blueprint('comments', __name__)
@@ -16,7 +16,7 @@ def new_comment():
         comment = Comment(
             # userId=form.data['userId'],  # console.log() and print() so I can find this line later
             userId=current_user.id,
-            postId=form.data['postId'],
+            commentId=form.data['commentId'],
             body=form.data['body']
         )
         db.session.add(comment)
@@ -69,3 +69,64 @@ def delete_comment(commentId):
 # def comment(commentId):
 #     comment = Comment.query.get(commentId)
 #     return comment.to_dict()
+
+
+@comment_routes.route('/<int:commentId>/vote', methods=['POST'])
+@login_required
+def new_vote(commentId):
+    vote = Vote.query.filter(Vote.userId==current_user.id) \
+                        .filter(Vote.commentId==commentId) \
+                        .filter(Vote.postId==None) \
+                        .first()
+    if vote:
+        return {'errors': ['conflicting vote found']}, 409
+    form = VoteForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        vote = Vote(
+            # userId=form.data['userId'],  # console.log() and print() so I can find this line later
+            userId=current_user.id,
+            commentId=commentId,
+            value=form.data['value']
+        )
+        db.session.add(vote)
+        db.session.commit()
+        return vote.to_dict()
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 400
+
+
+@comment_routes.route('/<int:commentId>/vote/<int:voteId>', methods=['PUT'])
+@login_required
+def edit_vote(commentId,voteId):
+    vote = Vote.query.get(voteId)
+    if not vote:
+        return {'errors': ["not found"]}, 404
+    if commentId != vote.commentId:
+        return {'errors': ["commentId doesn't match and I'm just disappointed in you"]}, 422
+    if vote.userId != current_user.id:
+        return {'errors': ["not yours"]}, 403
+    form = VoteForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        if form.data['value'] == vote.value:
+            return {'errors': ["value unchanged"]}, 422
+        vote.value = form.data['value']
+        # db.session.add(post)
+        db.session.commit()
+        return vote.to_dict()
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 400
+
+
+@comment_routes.route('/<int:commentId>/vote/<int:voteId>', methods=['DELETE'])
+@login_required
+def delete_vote(commentId, voteId):
+    vote = Vote.query.get(voteId)
+    if not vote:
+        return {'errors': ["not found"]}, 404
+    if commentId != vote.commentId:
+        return {'errors': ["commentId doesn't match and I'm just disappointed in you"]}, 422
+    if vote.userId != current_user.id:
+        return {'errors': ["not yours"]}, 403
+    db.session.delete(vote)
+    db.session.commit()
+    return jsonify(deleted=True)

commit fb46b39bed7df046a9ceb162594ba28eaab80128

fix when I broke new comments in 9e31bdeb by finding and replacing too much

app/api/comment_routes.py
@@ -16,7 +16,7 @@ def new_comment():
         comment = Comment(
             # userId=form.data['userId'],  # console.log() and print() so I can find this line later
             userId=current_user.id,
-            commentId=form.data['commentId'],
+            postId=form.data['postId'],
             body=form.data['body']
         )
         db.session.add(comment)