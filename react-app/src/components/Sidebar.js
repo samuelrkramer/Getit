1bcf0969ff5603579a652ce07d90967bb0326b0c
Sam Kramer <SamuelRKramer@gmail.com>
2023-02-13 14:48:45 -0500

    small explanation blurb in the devCard for those who don't know what old reddit is

--- react-app/src/components/Sidebar.js
+++ react-app/src/components/Sidebar.js
@@ -41,6 +41,9 @@ const Sidebar = () => {
       {sideAdd.map(el => (el))}
       <div className="sideSpaced devCard">
         <Logo />
+        <span class="devCardText">
+          is styled after <a href="https://old.reddit.com" target="_blank">Old Reddit</a>.
+        </span>
         <p>Check out the developer, who is eager to join your company.</p>
           <div className='devLinkBox'>
             <a href="https://github.com/samuelrkramer" target="_blank" rel="noreferrer">
